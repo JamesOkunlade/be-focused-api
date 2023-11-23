eec36456a73691cf0c2be4391338203f2797956a
James Okunlade <james.okunlade@gmail.com>
2023-11-23 02:36:17 +0100

    redux persist setup

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from '@reduxjs/toolkit';
 import thunk from 'redux-thunk';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
+import { PersistGate } from 'redux-persist/integration/react';
+
+
+
 
 import App from './components/App.jsx'
 import rootReducer from './store/reducers/index.js';
@@ -11,13 +17,36 @@ import './assets/styles/main.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 
+
+const persistConfig = {
+  key: 'root', // key is required
+  storage, // storage is required
+  // You can configure additional options here, such as blacklist or whitelist
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
+const store = createStore(persistedReducer, composeEnhancers(applyMiddleware(thunk)));
+
+// const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
+const persistor = persistStore(store);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <Provider store={store}>
+//       <App />
+//     </Provider>
+//   </React.StrictMode>,
+// );
